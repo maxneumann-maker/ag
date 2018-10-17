b275a7c479642be40346c66ec5d0bfbefdfbf41a
thomas-hilarius-meyer <thomas-hilarius-meyer@gmail.com>
2018-10-17 21:50:48 +0200

    Projekt DATENSICHTGERAET eröffnet

---
+++ perl/datensichtgeraet/datensichtgeraet.pl
@@ -0,0 +1,8 @@
+% Perl-Programm für ein Schulweites digitales Anzeigesystem "Datensichtgerät"
+
+    use strict;
+
+gwenview --geometry 500x800+50+50
+    okular --geometry 500x800+50+50
+    konqueror --geometry 500x800+50+50
+    calligrawords --geometry 100x100+450+450 datei % Paket koffice
